4ab165acbe5e514446852d8d9d628e8d7b0cf550
Laney Smith <lsmith.unc@gmail.com>
2019-08-29 10:59:04 -0600

    Updates

---
+++ precache-manifest.1d78b76ba47afbbf6294426f02873b1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a481937e3c6ff66ba13d598d418d805",
+    "url": "/@laney/react-use-truncate-list/index.html"
+  },
+  {
+    "revision": "f4ca9e15ffa1ca7d194e",
+    "url": "/@laney/react-use-truncate-list/static/css/main.4a4cc5e5.chunk.css"
+  },
+  {
+    "revision": "b1b111a576c21ca14192",
+    "url": "/@laney/react-use-truncate-list/static/js/2.ff289d6b.chunk.js"
+  },
+  {
+    "revision": "f4ca9e15ffa1ca7d194e",
+    "url": "/@laney/react-use-truncate-list/static/js/main.6e2fc574.chunk.js"
+  },
+  {
+    "revision": "7f8b98abcc52906deb06",
+    "url": "/@laney/react-use-truncate-list/static/js/runtime~main.04d5bbd6.js"
+  }
+]);
